2f68a4c3e9d503c25a09c502dc42950bfe9f74c4
ShakeebZ <shakeebzacky@gmail.com>
2021-11-30 16:59:25 -0800

    Created repo

---
+++ main.vhdl
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity fsm is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig
+    );
+end fsm;
+
+architecture rtl of fsm is
+
+begin
+
+end architecture;
