45b7c40b4eb18fb89f6d8b418627c43f7c78dbbb
ShakeebZ <shakeebzacky@gmail.com>
2021-12-03 15:42:01 -0800

    Control unit is finished and ControlUnitTesting works as intended

--- ASIP.vhdl
+++ ASIP.vhdl
@@ -7,6 +7,7 @@ entity ASIP is
         clkASIP, rstASIP, hard_rstASIP, stop_progASIP : in std_logic;
         programASIP : in std_logic_vector(3 DOWNTO 0);
         to_hex : out twoDArray;
+        pec_out : out std_logic_vector(3 DOWNTO 0)
 
     );
 end ASIP;
--- IO_Controller.vhdl
+++ IO_Controller.vhdl
@@ -5,7 +5,7 @@ USE work.definitions_package.all;
 
 entity IO_Controller is
     port (
-        toSeg : IN std_logic_vector(39 downto 0);
+        toSeg : IN twoDArray;
         toHex : OUT twoDArray
     );
 end IO_Controller;
@@ -20,13 +20,13 @@ architecture structure of IO_Controller is
     end component;
 begin
 
-    segDecoded0 : Custom7Seg PORT MAP(D => toSeg(4 DOWNTO 0), Y => toHex(0));
-    segDecoded1 : Custom7Seg PORT MAP(D => toSeg(9 DOWNTO 5), Y => toHex(1));
-    segDecoded2 : Custom7Seg PORT MAP(D => toSeg(14 DOWNTO 10), Y => toHex(2));
-    segDecoded3 : Custom7Seg PORT MAP(D => toSeg(19 DOWNTO 15), Y => toHex(3));
-    segDecoded4 : Custom7Seg PORT MAP(D => toSeg(24 DOWNTO 20), Y => toHex(4));
-    segDecoded5 : Custom7Seg PORT MAP(D => toSeg(29 DOWNTO 25), Y => toHex(5));
-    segDecoded6 : Custom7Seg PORT MAP(D => toSeg(34 DOWNTO 30), Y => toHex(6));
-    segDecoded7 : Custom7Seg PORT MAP(D => toSeg(39 DOWNTO 35), Y => toHex(7));
+    segDecoded0 : Custom7Seg PORT MAP(D => toSeg(0), Y => toHex(0));
+    segDecoded1 : Custom7Seg PORT MAP(D => toSeg(1), Y => toHex(1));
+    segDecoded2 : Custom7Seg PORT MAP(D => toSeg(2), Y => toHex(2));
+    segDecoded3 : Custom7Seg PORT MAP(D => toSeg(3), Y => toHex(3));
+    segDecoded4 : Custom7Seg PORT MAP(D => toSeg(4), Y => toHex(4));
+    segDecoded5 : Custom7Seg PORT MAP(D => toSeg(5), Y => toHex(5));
+    segDecoded6 : Custom7Seg PORT MAP(D => toSeg(6), Y => toHex(6));
+    segDecoded7 : Custom7Seg PORT MAP(D => toSeg(7), Y => toHex(7));
 
 end architecture;
